101d314ec6d1ff81d66bf9c4aaf6953de78491bc
PhiBuck <168071966+PhiBuck@users.noreply.github.com>
2024-09-17 11:57:05 +0200

    add sections and structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landingPage/landingPage.components';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    LandingPageComponent,
+    FooterComponent,
+    MainContentComponent,
+    ImprintComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/landingPage/landingPage.components.ts
+++ src/app/landingPage/landingPage.components.ts
@@ -1,13 +1,16 @@
 import { Component } from "@angular/core";
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 
 @Component({
     selector: 'app-landingPage',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, NavbarComponent],
     template: /*html*/`
     <section>
+        <app-navbar class="nav"></app-navbar>
         <h1 class="fontRaleway">SAKURA RAMEN</h1>
         <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
     </section>    
